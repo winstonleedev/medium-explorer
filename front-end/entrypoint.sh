61e4c00ed99df15c7d730ffd1cb505ec885c371e
Thanh Phu <thanh.phu@themedium.io>
2019-09-06 15:50:33 +0900

    Update way of assigning environments

--- front-end/entrypoint.sh
+++ front-end/entrypoint.sh
@@ -5,9 +5,9 @@ echo Setting environment...
 destFile=.env
 echo > "$destFile"
 for var in ${environment}; do
-  echo HASURA_HOST=$HASURA_HOST >> "$destFile"
-  echo HASURA_PORT=$HASURA_PORT >> "$destFile"
-  echo HASURA_ACCESS_KEY=$HASURA_ACCESS_KEY >> "$destFile"
+  echo "HASURA_HOST=$HASURA_HOST" >> "$destFile"
+  echo "HASURA_PORT=$HASURA_PORT" >> "$destFile"
+  echo "HASURA_ACCESS_KEY=$HASURA_ACCESS_KEY" >> "$destFile"
 done
 
 echo Installing packages...
