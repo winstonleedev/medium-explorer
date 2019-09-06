85867fbfadf2c943e71385c9b7d82a5ecf996e8e
Thanh Phu <thanh.phu@themedium.io>
2019-09-06 15:53:30 +0900

    Remove for loop

--- front-end/entrypoint.sh
+++ front-end/entrypoint.sh
@@ -4,11 +4,9 @@ echo Setting environment...
 
 destFile=.env
 echo > "$destFile"
-for var in ${environment}; do
-  echo "HASURA_HOST=$HASURA_HOST" >> "$destFile"
-  echo "HASURA_PORT=$HASURA_PORT" >> "$destFile"
-  echo "HASURA_ACCESS_KEY=$HASURA_ACCESS_KEY" >> "$destFile"
-done
+echo "HASURA_HOST=$HASURA_HOST" >> "$destFile"
+echo "HASURA_PORT=$HASURA_PORT" >> "$destFile"
+echo "HASURA_ACCESS_KEY=$HASURA_ACCESS_KEY" >> "$destFile"
 
 echo Installing packages...
 npm install --silent
